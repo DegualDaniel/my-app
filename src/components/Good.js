81f13a2b5d634ac6c5d8443ed03f9e7f269e296b
degual <danielawa53@gmail.com>
2023-08-23 00:38:08 -0300

    first react component

---
+++ src/components/Good.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Good (){
+//     return <h1> 
+//             hello degual  this is the functional components
+//     </h1>
+// }
+
+
+const Good = () =>   <h1>this ia a functional comoponent with arrow function</h1>
+ 
+
+
+
+
+export default Good
---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+class Welcome extends Component {
+    render(){
+        return <h2>
+            hello daniel this is a class components</h2>
+    }
+}
+
+export default Welcome
+
+
+
+
+
+
+
+
+
+
+
